5a32e5fda3dd5a8e6bba74eb560b4f4d11fb046d
Molly Zurheide <you@example.com>
2018-05-17 17:52:41 -0400

    working on spotify functionality

--- liri.js
+++ liri.js
@@ -2,47 +2,70 @@
 
 require("dotenv").config();
 var keys = require("keys.js");
+
 var Twitter = require("twitter");
-//var Spotify = require("node-spotify.api");
-//var omdbReq = require("omdb");
+var Spotify = require("node-spotify-api");
+var omdbReq = require("omdb");
+
+//KEYS
 var client = new Twitter(keys.twitter);
-//console.log(client) //works
-//var spotifyKeys = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
-//var tweetUrl = "https://api.twitter.com/1.1/statuses/user_timeline.json";
-/* tweetUrl += $.param({
+console.log(spotify) //NOTHING
+
+var twitterParams = {
     screen_name: "mjz_test",
     count: 10
-});
-console.log(tweetUrl);
-*/
-
-var params = {
-    screen_name: "mjz_test",
-    count: 10
-    //oauth_token: "VC76K39lVWgLJCcUrkyDJOzxO"
 };
 
-//"statuses/user_timeline"
-//https://api.twitter.com/1.1/statuses/lookup.json
-//https://api.twitter.com/1.1/statuses/user_timeline.json
-//http://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2&oauth_token=vZ9JO07zz0J5TaToGCrdyYeQT
-//id: 995734633852661766
-
-client.get("statuses/user_timeline", params, getTweets)
+client.get("statuses/user_timeline", twitterParams, getTweets)
 
 function getTweets(err, data, response) {
-    for (var i = 0; i<data.length; i++) {
-        //console.log("hi")
-        //console.log(data)
-        //console.log("heyyyy")
-        //var tweetRawArray = data;
-        console.log(data[i].text)
-        console.log(data[i].created_at)
+    for (var i = 0; i<20; i++) {
+        console.log(data[i].text);
+        console.log(data[i].created_at);
         var tweet = JSON.stringify(data[i]);
-        //console.log(tweet)
-        //console.log(response)
-        //console.log(data[0])
-        //console.log(tweetRaw)
     }
 }
+
+//for spotify
+var params = process.argv[3] + " " + process.argv[4] + " " + process.argv[5] + " " + process.argv[6] + " " + process.argv[7] + " " + process.argv[8] + " " + process.argv[9];
+params = params.trim();
+
+//console.log(params)
+
+function songSearch() {
+    Spotify.search({ //////////////?????????????????
+        type: 'track',
+        query: params
+    }).then (function(data) {
+        console.log(data);
+        console.log(data.tracks.items[0]);
+        var songInfo = data.tracks.items[0];
+        var artist = songInfo.artists[0].name;
+        var songName = songInfo.name;
+        var album = songInfo.album.name;
+        var preview = songInfo.preview_url;
+    })      
+};
+
+songSearch();
+
+
+/*
+function songRequest() {
+    Spotify.request("https://api.spotify.com/v1/tracks")
+    .then(function(response) {
+        console.log(response);
+        console.log(data.tracks.items[0]);
+    });
+};
+*/
+
+//songRequest();
+
+//request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx'
+//{
+//    type: 'track',
+//    query: 'All the Small Things'
+//search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
